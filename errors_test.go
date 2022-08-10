7e4f0f9dac4e41dc64edbaf2a8d2a0e806b9a00b
Jaime Soriano Pastor <jsorianopastor@gmail.com>
2022-08-10 21:08:47 +0200

    Detect unexpected fields

--- errors_test.go
+++ errors_test.go
@@ -2,73 +2,88 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
-	"regexp"
-	"strconv"
-	"strings"
 	"testing"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
 	cueerrors "cuelang.org/go/cue/errors"
 	cueyaml "cuelang.org/go/pkg/encoding/yaml"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHumanErrors(t *testing.T) {
-	schema := "foo: *0 | 1 | 2\nbar: int"
-	doc := "foo: 42\nbar: foo\nbaz: false\n"
+	cases := []struct {
+		Title    string
+		Schema   string
+		Doc      string
+		Expected []string
+	}{
+		{
+			Title:  "unexpected value",
+			Schema: "foo: 0 | 1 | 2",
+			Doc:    "foo: 42",
+			Expected: []string{
+				`document.yml:1:7: foo: found 42, expected one of: 0, 1`,
+			},
+		},
+		{
+			Title:  "wrong type",
+			Schema: "bar?: int",
+			Doc:    "bar: foo",
+			Expected: []string{
+				`document.yml:1:7: bar: conflicting values "foo" and int (mismatched types string and int)`,
+			},
+		},
+		{
+			Title:  "unexpected field",
+			Schema: "foo?: *0 | 1 | 2",
+			Doc:    "bar: 42",
+			Expected: []string{
+				`document.yml:1:2: bar: field not allowed`,
+			},
+		},
+		{
+			Title:  "two errors expected",
+			Schema: "foo: 0 | 1 | 2\nbar: int\n",
+			Doc:    "foo: 42\nbar: foo\n",
+			Expected: []string{
+				`document.yml:1:7: foo: found 42, expected one of: 0, 1`,
+				`document.yml:2:7: bar: conflicting values "foo" and int (mismatched types string and int)`,
+			},
+		},
+	}
 
-	cueCtx := cuecontext.New()
+	for _, c := range cases {
+		t.Run(c.Title, func(t *testing.T) {
+			cueCtx := cuecontext.New()
 
-	spec := cueCtx.CompileString(schema, cue.Filename("spec.cue"))
-	require.NoError(t, spec.Err())
+			// It seems that definitions need to be used to detect unexpected fields.
+			spec := cueCtx.CompileString("#Doc: {\n"+c.Schema+"\n}", cue.Filename("spec.cue")).LookupDef("Doc")
+			require.NoError(t, spec.Err())
 
-	expr, err := cueyaml.Unmarshal([]byte(doc))
-	require.NoError(t, err)
+			expr, err := cueyaml.Unmarshal([]byte(c.Doc))
+			require.NoError(t, err)
 
-	v := cueCtx.BuildExpr(expr, cue.Filename("document.yml"))
-	v = v.Unify(spec)
-	errs := v.Validate(cue.Concrete(true))
-	for i, err := range ValidationErrors("document.yml", errs) {
-		t.Logf("#%d: %v", i, cueerrors.Details(err, nil))
+			v := spec.Context().BuildExpr(expr, cue.Filename("document.yml"))
+			v = v.Unify(spec)
+			errs := v.Validate(cue.Concrete(true))
+			assert.NotEmpty(t, errs)
+
+			formatted := ValidationErrors("document.yml", errs)
+			for i, err := range formatted {
+				t.Logf("#%d: %v", i, cueerrors.Details(err, nil))
+			}
+
+			assert.EqualValues(t, c.Expected, errorsToStrings(formatted))
+		})
 	}
 }
 
-var (
-	reEmptyDisjunctionErr  = regexp.MustCompile(`^(.*): (\d+) errors in empty disjunction`)
-	reConflictingValuesErr = regexp.MustCompile(`^(.*): conflicting values (.*) and (.*)`)
-)
-
-func ValidationErrors(filename string, err error) []error {
-	var result []error
-	for i, errs := 0, cueerrors.Errors(err); i < len(errs); i++ {
-		e := errs[i]
-		if m := reEmptyDisjunctionErr.FindStringSubmatch(e.Error()); len(m) > 0 {
-			field := string(m[1])
-			n, _ := strconv.Atoi(string(m[2]))
-			pos := errs[i+1].InputPositions()[0]
-			var expected []string
-			var found string
-			for _, conflict := range errs[i+1 : i+n] {
-				m := reConflictingValuesErr.FindStringSubmatch(conflict.Error())
-				expected = append(expected, string(m[2]))
-				if found == "" {
-					found = string(m[3])
-				}
-			}
-			err := fmt.Errorf("%s:%d:%d: %s: found %s, expected one of: %s",
-				filename, pos.Line(), pos.Column(), field,
-				found, strings.Join(expected, ", "),
-			)
-			result = append(result, err)
-			i += n
-			continue
-		}
-
-		pos := e.InputPositions()[0]
-		err := fmt.Errorf("%s:%d:%d: %s", filename, pos.Line(), pos.Column(), e)
-		result = append(result, err)
+func errorsToStrings(errs []error) []string {
+	var formatted []string
+	for _, err := range errs {
+		formatted = append(formatted, err.Error())
 	}
-	return result
+	return formatted
 }
